87e353cfde9ef66ab42d3e70d41f0ee9127a99dd
David Morales <git@damorales.com>
2020-06-30 10:43:52 -0500

    array_unique

---
+++ exercises/array_unique/index.js
@@ -0,0 +1,24 @@
+/*
+  Given an array of nums, remove the duplicates
+  such that each element appear only once.
+
+  [1,2,1]   --> [1,2]
+  [1,1,1]   --> [1]
+  [0,0,0,1] --> [0,1]
+*/
+
+const solution = require('./solution');
+
+function array_unique(arr) {
+  /*
+    Remove the function body and write
+    your own solution in place.
+  */
+  return solution(...arguments);
+}
+
+const result = array_unique([1,1,2]);
+console.log('Output:', result);
+
+module.exports = array_unique;
+
---
+++ exercises/array_unique/test.js
@@ -0,0 +1,32 @@
+const array_unique = require('./');
+
+test('array_unique function is defined', () => {
+  expect(typeof array_unique).toEqual('function');
+});
+
+test('calculates unique elements for []', () => {
+  expect(array_unique([])).toEqual([]);
+});
+
+test('calculates unique elements for [-1]', () => {
+  expect(array_unique([-1])).toEqual([-1]);
+});
+
+test('calculates unique elements for [1]', () => {
+  expect(array_unique([1])).toEqual([1]);
+});
+
+test('calculates unique elements for [5,5]', () => {
+  expect(array_unique([5,5])).toEqual([5]);
+});
+
+test('calculates unique elements for [1,1,2]', () => {
+  expect(array_unique([1,1,2])).toEqual([1,2]);
+});
+
+test('calculates unique elements for [0,1,-1,0,1,-1]', () => {
+  expect(array_unique([0,1,-1,0,1,-1])).toEqual([0,1,-1]);
+});
+
+
+
